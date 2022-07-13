62c9348dc7e8228483c4b47cc186271f850af1cd
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2022-07-13 13:07:56 +0200

    Updating variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,6 @@
 
 provider "aws" {
     region     = "us-east-1"
-    access_key = "AKIA4QGDHDEOCTAKX2UK"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
